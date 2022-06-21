ffc29662f90f2dd030b956181ce560019bd3bfd8
maibelline <mayb80@hotmail.fr>
2022-06-21 11:49:59 +0200

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
  //TIMELINE 
- (function () {
+(function () {
 	"use strict";
-  
+
 
 	var items = document.querySelectorAll(".timeline li");
 
 
 	function isElementInViewport(el) {
-	  var rect = el.getBoundingClientRect();
-	  return (
-		rect.top >= 0 &&
-		rect.left >= 0 &&
-		rect.bottom <=
-		  (window.innerHeight || document.documentElement.clientHeight) &&
-		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-	  );
+	var rect = el.getBoundingClientRect();
+	return (
+	rect.top >= 0 &&
+	rect.left >= 0 &&
+	rect.bottom <=
+		(window.innerHeight || document.documentElement.clientHeight) &&
+	rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
+function callbackFunc() {
+	for (var i = 0; i < items.length; i++) {
+	if (isElementInViewport(items[i])) {
+		items[i].classList.add("in-view");
 	}
-  
-	function callbackFunc() {
-	  for (var i = 0; i < items.length; i++) {
-		if (isElementInViewport(items[i])) {
-		  items[i].classList.add("in-view");
-		}
-	  }
 	}
-  
+}
+
 	// listen for events
 	window.addEventListener("load", callbackFunc);
 	window.addEventListener("resize", callbackFunc);
 	window.addEventListener("scroll", callbackFunc);
-  })();
+})();
 
 
 // // // MODAL
@@ -41,22 +41,22 @@ const buttonClose = document.getElementsByClassName('modalClose')[0];
 buttonOpen.addEventListener('click', modalOpen);
 function modalOpen() {
 //   modal.style.display = 'block';
-  modal.classList.add('effect');
+	modal.classList.add('effect');
 };
 
 //
 buttonClose.addEventListener('click', modalClose);
 function modalClose() {
 //   modal.style.display = 'none';
-  modal.classList.remove('effect');
+	modal.classList.remove('effect');
 };
 
 //
 addEventListener('click', outsideClose);
 function outsideClose(e) {
-  if (e.target == modal) {
+if (e.target == modal) {
 //   modal.style.display = 'none';
-  modal.classList.remove('effect');
-  };
+	modal.classList.remove('effect');
+    };
 };
 
